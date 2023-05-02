f4b614448883c400e624cb7fff667518c2f28cc5
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2023-05-02 15:55:32 -0300

    Dando procedimento ao desenvolvimento da Controller do módulo de Correntista.

--- App/Controller/CorrentistaController.php
+++ App/Controller/CorrentistaController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Exception;
 
+use App\Model\CorrentistaModel;
+
 class CorrentistaController extends Controller
 {
 
@@ -13,14 +15,28 @@ class CorrentistaController extends Controller
         try
         {
 
+            $objeto_json = json_decode(file_get_contents("php://input"));
 
+            $model = new CorrentistaModel();
+
+            $model->id = $objeto_json->id;
+
+            $model->cpf = $objeto_json->cpf;
+
+            $model->data_nascimento = $objeto_json->data_nascimento;
+
+            $model->senha_correntista = $objeto_json->senha_correntista;
+
+            $model->ativo = $objeto_json->ativo;
+
+            parent::GetResponseAsJSON($model->Save());
 
         }
 
         catch(Exception $ex)
         {
 
-            
+            parent::GetExceptionAsJSON($ex);
 
         }
 
@@ -32,14 +48,16 @@ class CorrentistaController extends Controller
         try
         {
 
+            $id = json_decode(file_get_contents("php://input"));
 
+            (new CorrentistaModel())->Erase((int) $id);
 
         }
 
         catch(Exception $ex)
         {
 
-            
+            parent::GetExceptionAsJSON($ex);
 
         }
 
@@ -51,14 +69,18 @@ class CorrentistaController extends Controller
         try
         {
 
+            $model = new CorrentistaModel();
 
+            $model->Query();
+
+            parent::GetResponseAsJSON($model->rows);
 
         }
 
         catch(Exception $ex)
         {
 
-            
+            parent::GetExceptionAsJSON($ex);
 
         }
 
@@ -70,14 +92,20 @@ class CorrentistaController extends Controller
         try
         {
 
+            $model = new CorrentistaModel();
 
+            $conteudo = json_decode(file_get_contents("php://input"));
+
+            $model->Query($conteudo);
+
+            parent::GetResponseAsJSON($model->rows);
 
         }
 
         catch(Exception $ex)
         {
 
-            
+            parent::GetExceptionAsJSON($ex);
 
         }
 
