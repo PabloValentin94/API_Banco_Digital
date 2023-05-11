3c215ebd13bedea0fdedb4c828841214ea915464
Pablo Valentin <95161515+PabloValentin94@users.noreply.github.com>
2023-05-11 15:58:21 -0300

    Update Config.php

--- App/Config.php
+++ App/Config.php
@@ -2,7 +2,7 @@
 
 define("BASEDIR", dirname(__FILE__, 2) . "/");
 
-$_ENV["database"]["host"] = "localhost:3306";
+$_ENV["database"]["host"] = "localhost:3307";
 $_ENV["database"]["user"] = "root";
 $_ENV["database"]["password"] = "etecjau";
 $_ENV["database"]["db_name"] = "db_banco_digital";
